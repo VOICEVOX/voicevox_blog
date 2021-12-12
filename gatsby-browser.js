859f733df32cd27162332e979200b5383fa95a78
牛蒡 <53455523+Gobo-nico@users.noreply.github.com>
2021-12-12 20:36:49 +0900

    fontawesomeの表示がちらつく不具合を修正 (#11)
    * fontawesomeの表示が一瞬崩れる不具合を修正
    
    * font-faceでGoogle Fontsを読み込む
    
    * font-daceを調整
    
    * font-faceを調整
    
    * font-faceの調整
    
    * Update layout.scss
    
    Co-authored-by: 牛蒡 <Gobo-nico@outlook.jp>

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,6 @@
  */
 
 // You can delete this file if you're not using it
+import "@fortawesome/fontawesome-svg-core/styles.css"
+import { config } from '@fortawesome/fontawesome-svg-core'
+config.autoAddCss = false
