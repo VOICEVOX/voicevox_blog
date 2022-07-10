06367d5f9a2ff16acdf7e3e6f9c3208efa2e5530
Hiroshiba <hihokaruta@gmail.com>
2022-07-10 20:16:58 +0900

    0.12.4

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -413,7 +413,7 @@ const Main: React.FC<{ setShowingHeader: (show: boolean) => void }> = ({
                   </span>
                   <span className="has-text-weight-semibold">ダウンロード</span>
                 </a>
-                <p className="is-align-self-center is-size-6">Version 0.12.3</p>
+                <p className="is-align-self-center is-size-6">Version 0.12.4</p>
               </div>
             </div>
           </section>
