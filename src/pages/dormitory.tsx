d5d5a20f3e6aa4613d620eaaf70ccf22bfd6d80f
Hiroshiba <hihokaruta@gmail.com>
2023-03-13 05:29:30 +0900

    ボイボ寮のキャラ個別ページからダウンロードを可能にする

--- src/pages/dormitory.tsx
+++ src/pages/dormitory.tsx
@@ -13,6 +13,8 @@ type DormitoryProps = {
   setShowingHeader: (show: boolean) => void
 }
 
+// FIXME: ハッシュ付きURLに遷移してページ移動したあとブラウザバックするとそのハッシュ位置に戻ってしまうバグを防ぎたい
+// https://github.com/VOICEVOX/voicevox_blog/issues/104
 const Dormitory: React.FC<DormitoryProps> = ({ setShowingHeader }) => {
   const { characterInfos } = useDetailedCharacterInfo()
 
