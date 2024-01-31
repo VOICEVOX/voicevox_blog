b3eb0df1ff4884af6af7a99bbb94c16719332ab1
Hiroshiba <hihokaruta@gmail.com>
2024-01-31 22:58:00 +0900

    to 0.16.1 (#186)

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 import { CharacterKey } from "./types/dormitoryCharacter"
 
-export const APP_VERSION = "0.16.0"
+export const APP_VERSION = "0.16.1"
 export const NEMO_VERSION = "0.14.0"
 
 export const characterKeys: CharacterKey[] = [
