d8749567bfb287cee1e366ec740b4d3c3f6f5add
Hiroshiba <hihokaruta@gmail.com>
2024-03-09 13:25:46 +0900

    to 0.17.1 (#193)

--- src/scripts/linuxInstallNvidia.sh
+++ src/scripts/linuxInstallNvidia.sh
@@ -36,6 +36,6 @@ EOS
     exit 1
 fi
 
-curl -fsSL https://raw.githubusercontent.com/VOICEVOX/voicevox/0.16.1/build/installer_linux.sh >tmp_voicevox_installer.sh
-VERSION=0.16.1 NAME=linux-nvidia-appimage bash tmp_voicevox_installer.sh
+curl -fsSL https://raw.githubusercontent.com/VOICEVOX/voicevox/0.17.1/build/installer_linux.sh >tmp_voicevox_installer.sh
+VERSION=0.17.1 NAME=linux-nvidia-appimage bash tmp_voicevox_installer.sh
 rm tmp_voicevox_installer.sh
