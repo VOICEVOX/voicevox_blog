c44d6ca0bee29fc80f65af9ed27a764b597b052c
Hiroshiba <hihokaruta@gmail.com>
2024-06-25 07:23:11 +0900

    フォーマット (#211)

--- src/pages/news/index.tsx
+++ src/pages/news/index.tsx
@@ -1,16 +1,16 @@
+import { Link, graphql, useStaticQuery } from "gatsby"
 import React from "react"
 import "../../components/layout.scss"
 import { Page } from "../../components/page"
 import Seo from "../../components/seo"
-import { Link, graphql, useStaticQuery } from "gatsby"
 import shareThumb from "../../images/nemo/share-thumbnail.png"
 
 const NewsIndex = () => {
   const data = useStaticQuery<Queries.IndexPageQuery>(graphql`
     query IndexPage {
-      allMarkdownRemark (
-        filter: {fileAbsolutePath: {regex: "/news/"}}
-        sort: {frontmatter: {date: DESC}}
+      allMarkdownRemark(
+        filter: { fileAbsolutePath: { regex: "/news/" } }
+        sort: { frontmatter: { date: DESC } }
       ) {
         edges {
           node {
@@ -24,7 +24,7 @@ const NewsIndex = () => {
         }
       }
     }
-  `);
+  `)
 
   return (
     <Page>
@@ -36,13 +36,15 @@ const NewsIndex = () => {
       />
       <section className="section">
         <div className="container is-max-desktop">
-        <h1 className="title">ニュース</h1>
-          {data.allMarkdownRemark.edges.map((edge) => (
+          <h1 className="title">ニュース</h1>
+          {data.allMarkdownRemark.edges.map(edge => (
             <div key={edge.node.frontmatter!.slug} className="mb-3">
               <Link to={`/news/${edge.node.frontmatter!.slug}`}>
                 {edge.node.frontmatter!.title}
               </Link>
-              <p className="has-text-grey-light">{edge.node.frontmatter!.date}</p>
+              <p className="has-text-grey-light">
+                {edge.node.frontmatter!.date}
+              </p>
             </div>
           ))}
         </div>
