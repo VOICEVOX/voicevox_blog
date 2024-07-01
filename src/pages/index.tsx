faa21059c2bee6d3cb876848e277bc6c168f7549
jdkfx <amateurranch@gmail.com>
2024-07-02 02:09:15 +0900

    不要なimportを削除

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,3 @@
-import { faGithub } from "@fortawesome/free-brands-svg-icons"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { Link } from "gatsby"
 import { GatsbyImage } from "gatsby-plugin-image"
 import React, { useContext, useEffect, useRef, useState } from "react"
