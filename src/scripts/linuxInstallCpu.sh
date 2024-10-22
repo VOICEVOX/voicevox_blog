792eec67061842828f04879fd362e32bbf28d748
Nanashi. <sevenc7c@sevenc7c.com>
2024-10-23 00:29:23 +0900

    Fix: Linux版インストーラーのバージョンの不一致を修正 (#225)

--- src/scripts/linuxInstallCpu.sh
+++ src/scripts/linuxInstallCpu.sh
@@ -36,6 +36,6 @@ EOS
     exit 1
 fi
 
-curl -fsSL https://raw.githubusercontent.com/VOICEVOX/voicevox/0.20.0/build/installer_linux.sh >tmp_voicevox_installer.sh
-VERSION=0.20.0 NAME=linux-cpu-appimage bash tmp_voicevox_installer.sh
+curl -fsSL https://raw.githubusercontent.com/VOICEVOX/voicevox/0.21.0/build/installer_linux.sh >tmp_voicevox_installer.sh
+VERSION=0.21.0 NAME=linux-cpu-appimage bash tmp_voicevox_installer.sh
 rm tmp_voicevox_installer.sh
