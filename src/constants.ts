b248e08e5464a24f779396a680517783cf4dc5d7
Hiroshiba <hihokaruta@gmail.com>
2024-10-26 18:46:40 +0900

    バージョンを上げる

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 import { CharacterKey } from "./types/dormitoryCharacter"
 
-export const APP_VERSION = "0.21.0"
+export const APP_VERSION = "0.21.1"
 export const NEMO_VERSION = "0.21.0"
 
 export const characterKeys: CharacterKey[] = [
