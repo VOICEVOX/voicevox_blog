36ecb1af8147777cd2f730b5b79f7a0d3e2e7e9e
Hiroshiba <hihokaruta@gmail.com>
2025-04-08 09:27:01 +0900

    feat!: 製品ページで選んだキャラクターが即座に中央に表示するようにし、テストも追加 (#271)

---
+++ tests/e2e/ui/product.spec.ts
@@ -0,0 +1,41 @@
+import { gotoAndWait } from "../helper";
+import { characterEntries, characterKeys } from "@/constants/characterEntry";
+import { getProductPageUrl } from "@/constants/url";
+import { expect, test } from "@playwright/test";
+
+// NOTE: https://github.com/VOICEVOX/voicevox_blog/pull/267#pullrequestreview-2740302454
+test("後ろの方のキャラクターの製品ページに遷移したとき、キャラクターリスト内のそのキャラクターが表示されている", async ({
+  page,
+}) => {
+  const lastCharacterKey = characterKeys[characterKeys.length - 1];
+  const lastCharacterEntry = characterEntries[lastCharacterKey];
+
+  await gotoAndWait(page, getProductPageUrl(lastCharacterEntry));
+
+  const characterList = page.getByLabel("キャラクター一覧");
+  await expect(
+    characterList.getByLabel(lastCharacterEntry.name, { exact: true }),
+  ).toBeInViewport();
+});
+
+test("キャラクターの製品ページでは、そのキャラクターが中央に表示される", async ({
+  page,
+}) => {
+  // 真ん中の方のキャラクターを選択
+  const middleIndex = Math.floor(characterKeys.length / 2);
+  const middleCharacterKey = characterKeys[middleIndex];
+  const middleCharacterEntry = characterEntries[middleCharacterKey];
+
+  // 真ん中のキャラクターのページへ遷移
+  await gotoAndWait(page, getProductPageUrl(middleCharacterEntry));
+
+  // キャラメニューリストのスクロール位置が大体真ん中になっていることを確認
+  const characterList = page.getByLabel("キャラクター一覧");
+  const position = await characterList.evaluate((el) => el.scrollLeft);
+  const scrollWidth = await characterList.evaluate((el) => el.scrollWidth);
+  const clientWidth = await characterList.evaluate((el) => el.clientWidth);
+  const middlePosition = scrollWidth / 2 - clientWidth / 2;
+  const tolerance = clientWidth / 8;
+  expect(position).toBeGreaterThan(middlePosition - tolerance);
+  expect(position).toBeLessThan(middlePosition + tolerance);
+});
