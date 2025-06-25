6995e2f51f9d2fb8507c7d4defa8ade3f597bba9
Hiroshiba <hihokaruta@gmail.com>
2025-06-25 15:47:21 +0900

    feat!: to 0.24.0 (#286)

--- src/assets/script/linuxInstallCpuX64.sh
+++ src/assets/script/linuxInstallCpuX64.sh
@@ -37,5 +37,5 @@ EOS
 fi
 
 curl -fsSL https://raw.githubusercontent.com/VOICEVOX/voicevox/0.24.0/build/installer_linux.sh >tmp_voicevox_installer.sh
-VERSION=0.24.0 NAME=linux-nvidia-appimage bash tmp_voicevox_installer.sh
+VERSION=0.24.0 NAME=linux-cpu-x64-appimage bash tmp_voicevox_installer.sh
 rm tmp_voicevox_installer.sh
