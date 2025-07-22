8a42b2f869ab81c1c09440a7bce5ec8f3a9dfdbc
Hiroshiba <hihokaruta@gmail.com>
2025-07-22 16:22:40 +0900

    fix!: ソングとしてハミング音声が表示されていたので修正します。 (#296)

--- src/constants/characterInfos/helper.ts
+++ src/constants/characterInfos/helper.ts
@@ -12,29 +12,46 @@ import {
 } from "@/constants/characaterAsset";
 import type { CharacterKey } from "@/constants/characterEntry";
 
+function getBasename(filePath: string): string {
+  return filePath.split("/").pop() || filePath.split("\\").pop() || filePath;
+}
+
 /** トーク用音声をスタイルごとに分ける */
 function makeTalkVoiceAudios(key: CharacterKey, styles: Styles) {
   return styles
     .filter((v) => v.type == "talk")
-    .map((v) => ({
-      style: v.name,
-      urls: talkAudiosAndPaths[key]
-        .filter((obj) => obj.path.includes(v.id))
-        .map((obj) => obj.asset),
-    }));
+    .map((v) => {
+      const urls = talkAudiosAndPaths[key]
+        .filter((obj) => getBasename(obj.path).includes(v.id))
+        .map((obj) => obj.asset);
+      if (urls.length !== 0 && urls.length !== 3) {
+        throw new Error(
+          `キャラクター「${key}」の音声（スタイル：${v.name}、タイプ：${v.type}）の数が0または3ではありません。現在の数：${urls.length}`,
+        );
+      }
+      return { style: v.name, urls };
+    });
 }
 
 /** ソング音声をスタイルごとに分ける */
 function makeSongVoiceAudios(key: CharacterKey, styles: Styles) {
   return styles
     .filter((v) => ["song", "humming"].includes(v.type))
-    .map((v) => ({
-      style: v.name,
-      styleType: v.type as "song" | "humming",
-      urls: songAudiosAndPaths[key]
-        .filter((obj) => obj.path.includes(v.id))
-        .map((obj) => obj.asset),
-    }));
+    .map((v) => {
+      const urls = songAudiosAndPaths[key]
+        .filter(
+          (obj) =>
+            getBasename(obj.path).includes(v.id) &&
+            getBasename(obj.path).includes(v.type),
+        )
+        .map((obj) => obj.asset);
+      if (urls.length !== 0 && urls.length !== 3) {
+        throw new Error(
+          `キャラクター「${key}」の音声（スタイル：${v.name}、タイプ：${v.type}）の数が0または3ではありません。現在の数：${urls.length}`,
+        );
+      }
+      return { style: v.name, styleType: v.type as "song" | "humming", urls };
+    });
 }
 
 /** キャラクターごとのアセットを取得する */
