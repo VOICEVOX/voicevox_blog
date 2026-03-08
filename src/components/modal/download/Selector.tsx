e8b74d4b209e33d2e0ee427aa8838897da9a92a5
Hiroshiba <hihokaruta@gmail.com>
2026-03-09 05:48:46 +0900

    style(ui): ボタンのhoverスタイルを統一し、focusスタイルをデフォルトに変更

--- src/components/modal/download/Selector.tsx
+++ src/components/modal/download/Selector.tsx
@@ -22,8 +22,8 @@ export default function Selector<T extends string>({
               key={index}
               className={
                 isSelected
-                  ? "vv-status-layer focus:ring-primary/50 px-xl inline-flex items-center justify-center rounded-full border-none bg-emerald-400 py-2.5 text-base font-semibold text-black focus:ring-2 focus:ring-offset-2 focus:outline-none"
-                  : "vv-status-layer focus:ring-primary/50 px-xl inline-flex items-center justify-center rounded-full border border-neutral-300 bg-white py-2.5 text-base font-normal text-black shadow-sm focus:ring-2 focus:ring-offset-2 focus:outline-none"
+                  ? "vv-status-layer px-xl inline-flex items-center justify-center rounded-full border-none bg-emerald-400 py-2.5 text-base font-semibold text-black"
+                  : "vv-status-layer px-xl inline-flex items-center justify-center rounded-full border border-neutral-300 bg-white py-2.5 text-base font-normal text-black shadow-sm"
               }
               onClick={() => setSelected(candidate)}
               type="button"
