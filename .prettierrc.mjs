934acc980b9bb173e1f05330e02df1b3788ec7ca
Hiroshiba <hihokaruta@gmail.com>
2026-09-07 21:55:52 +0000

    fix: Astro の import 整列を修正 (#390)

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -5,8 +5,8 @@ export default {
   plugins: [
     "@trivago/prettier-plugin-sort-imports",
     "prettier-plugin-astro",
-    "prettier-plugin-astro-organize-imports",
     "prettier-plugin-tailwindcss",
+    "prettier-plugin-astro-organize-imports", // 他のプラグインの整形もまとめて実行するので、最後に指定する
   ],
   overrides: [
     {
